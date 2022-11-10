885c44ee606ba175be49e40d54a465dab9eca886
yanuar-ar <yanuar.ar@gmail.com>
2022-11-10 19:54:50 +0700

    add unpause

---
+++ packages/nouns-contracts/tasks/unpause.ts
@@ -0,0 +1,17 @@
+import { task, types } from 'hardhat/config';
+
+task('unpause', 'Unpause auction')
+  .addOptionalParam(
+    'auctionHouseProxy',
+    'The `auctionHouseProxy` contract address',
+    '0x04f83161B21a5B6DE987b5056d48F72DdB8deC8f',
+    types.string,
+  )
+  .setAction(async ({ auctionHouseProxy }, { ethers }) => {
+    const auctionHouseFactory = await ethers.getContractFactory('AlpsAuctionHouse');
+    const auctionHouseContract = auctionHouseFactory.attach(auctionHouseProxy);
+
+    await (await auctionHouseContract.unpause()).wait();
+
+    console.log(`Success unpause.`);
+  });
